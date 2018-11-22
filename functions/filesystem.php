04e6eefd133a006e074d34f57ee2eed87cdb9542
lipemat <mat@matlipe.com>
2018-11-22 13:19:15 -0500

    support custom directories for generated files (like wp as a submodule)

--- functions/filesystem.php
+++ functions/filesystem.php
@@ -3,7 +3,7 @@
  * Filesystem Operations
  *
  * @since 1.0
- * 
+ *
  * @function	superpwa_wp_filesystem_init()	Initialize the WP filesystem
  * @function	superpwa_put_contents()			Write to a file using WP_Filesystem() functions
  * @function	superpwa_get_contents()			Read contents of a file using WP_Filesystem() functions
@@ -19,9 +19,9 @@ if ( ! defined('ABSPATH') ) exit;
  * @since 1.0
  */
 function superpwa_wp_filesystem_init() {
-	
+
 	global $wp_filesystem;
-	
+
 	if ( empty( $wp_filesystem ) ) {
 		require_once( trailingslashit( ABSPATH ) . 'wp-admin/includes/file.php' );
 		WP_Filesystem();
@@ -29,29 +29,54 @@ function superpwa_wp_filesystem_init() {
 }
 
 /**
+ * Get the path to be used to generate files
+ *
+ * @author Mat Lipe
+ * @since  1.9.1
+ *
+ * @return string
+ */
+function superpwa_get_output_path() {
+	return apply_filters( 'superpwa_output_path', trailingslashit( ABSPATH ) );
+}
+
+/**
+ * Get the url to be used to retrieve generate files
+ *
+ * @author Mat Lipe
+ * @since  1.9.1
+ *
+ * @return string
+ */
+function superpwa_get_output_url() {
+	return apply_filters( 'superpwa_output_url', trailingslashit( network_site_url() ) );
+}
+
+
+/**
  * Write to a file using WP_Filesystem() functions
  *
  * @param	$file		Filename with path
  * @param	$content	Contents to be written to the file. Default null
  * @return	True on success, false if file isn't passed or if writing failed.
- * 
+ *
  * @since	1.0
  */
 function superpwa_put_contents( $file, $content = null ) {
-	
+
 	// Return false if no filename is provided
 	if ( empty( $file ) ) {
 		return false;
 	}
-	
+
 	// Initialize the WP filesystem
 	superpwa_wp_filesystem_init();
 	global $wp_filesystem;
-	
+
 	if( ! $wp_filesystem->put_contents( $file, $content, 0644) ) {
 		return false;
 	}
-	
+
 	return true;
 }
 
@@ -61,47 +86,47 @@ function superpwa_put_contents( $file, $content = null ) {
  * @param	$file	Filename with path.
  * @param	$array	Set true to return read data as an array. False by default.
  * @return	(string|bool) The function returns the read data or false on failure.
- * 
+ *
  * @since 	1.0
  */
 function superpwa_get_contents( $file, $array = false ) {
-	
+
 	// Return false if no filename is provided
 	if ( empty( $file ) ) {
 		return false;
 	}
-	
+
 	// Initialize the WP filesystem
 	superpwa_wp_filesystem_init();
 	global $wp_filesystem;
-	
+
 	// Reads entire file into a string
 	if ( $array == false ) {
 		return $wp_filesystem->get_contents( $file );
 	}
-	
+
 	// Reads entire file into an array
 	return $wp_filesystem->get_contents_array( $file );
 }
 
 /**
  * Delete a file
- * 
+ *
  * @param	$file	Filename with path
  * @return	bool	True on success, false otherwise
- * 
+ *
  * @since	1.0
  */
 function superpwa_delete( $file ) {
-	
+
 	// Return false if no filename is provided
 	if ( empty( $file ) ) {
 		return false;
 	}
-	
+
 	// Initialize the WP filesystem
 	superpwa_wp_filesystem_init();
 	global $wp_filesystem;
-	
+
 	return $wp_filesystem->delete( $file );
 }
--- public/sw.php
+++ public/sw.php
@@ -3,7 +3,7 @@
  * Service worker related functions of SuperPWA
  *
  * @since 1.0
- * 
+ *
  * @function	superpwa_sw()					Service worker filename, absolute path and link
  * @function	superpwa_generate_sw()			Generate and write service worker into sw.js
  * @function	superpwa_sw_template()			Service worker tempalte
@@ -26,31 +26,31 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  *				src for relative link to service worker (replaces SUPERPWA_SW_SRC). Default value
  *
  * @return (string) filename, absolute path or link to manifest.
- * 
+ *
  * @since 1.6
  * @since 1.7 src to service worker is made relative to accomodate for domain mapped multisites.
  * @since 1.8 Added filter superpwa_sw_filename.
  */
 function superpwa_sw( $arg = 'src' ) {
-	
+
 	$sw_filename = apply_filters( 'superpwa_sw_filename', 'superpwa-sw' . superpwa_multisite_filename_postfix() . '.js' );
-	
+
 	switch( $arg ) {
-		
+
 		// Name of service worker file
 		case 'filename':
 			return $sw_filename;
 			break;
-		
-		// Absolute path to service worker. SW must be in the root folder	
+
+		// Absolute path to service worker. SW must be in the root folder
 		case 'abs':
-			return trailingslashit( ABSPATH ) . $sw_filename;
+			return superpwa_get_output_path() . $sw_filename;
 			break;
-		
+
 		// Link to service worker
 		case 'src':
 		default:
-			return parse_url( trailingslashit( network_site_url() ) . $sw_filename, PHP_URL_PATH );
+			return parse_url( superpwa_get_output_url() . $sw_filename, PHP_URL_PATH );
 			break;
 	}
 }
@@ -59,24 +59,24 @@ function superpwa_sw( $arg = 'src' ) {
  * Generate and write service worker into superpwa-sw.js
  *
  * @return (boolean) true on success, false on failure.
- * 
+ *
  * @since 1.0
  */
 function superpwa_generate_sw() {
-	
+
 	// Get Settings
 	$settings = superpwa_get_settings();
-	
+
 	// Get the service worker tempalte
 	$sw = superpwa_sw_template();
-	
+
 	// Delete service worker if it exists
 	superpwa_delete_sw();
-	
+
 	if ( ! superpwa_put_contents( superpwa_sw( 'abs' ), $sw ) ) {
 		return false;
 	}
-	
+
 	return true;
 }
 
@@ -84,16 +84,16 @@ function superpwa_generate_sw() {
  * Service Worker Tempalte
  *
  * @return (string) Contents to be written to superpwa-sw.js
- * 
+ *
  * @since 1.0
  * @since 1.7 added filter superpwa_sw_template
  * @since 1.9 added filter superpwa_sw_files_to_cache
  */
 function superpwa_sw_template() {
-	
+
 	// Get Settings
 	$settings = superpwa_get_settings();
-	
+
 	// Start output buffer. Everything from here till ob_get_clean() is returned
 	ob_start();  ?>
 'use strict';
@@ -102,7 +102,7 @@ function superpwa_sw_template() {
  * Service Worker of SuperPWA
  * To learn more and add one to your website, visit - https://superpwa.com
  */
- 
+
 const cacheName = '<?php echo parse_url( get_bloginfo( 'wpurl' ), PHP_URL_HOST ) . '-superpwa-' . SUPERPWA_VERSION; ?>';
 const startPage = '<?php echo superpwa_get_start_url(); ?>';
 const offlinePage = '<?php echo get_permalink( $settings['offline_page'] ) ? superpwa_httpsify( get_permalink( $settings['offline_page'] ) ) : superpwa_httpsify( get_bloginfo( 'wpurl' ) ); ?>';
@@ -142,21 +142,21 @@ self.addEventListener('activate', function(e) {
 
 // Fetch
 self.addEventListener('fetch', function(e) {
-	
+
 	// Return if the current request url is in the never cache list
 	if ( ! neverCacheUrls.every(checkNeverCacheList, e.request.url) ) {
 	  console.log( 'SuperPWA: Current request is excluded from cache.' );
 	  return;
 	}
-	
+
 	// Return if request url protocal isn't http or https
 	if ( ! e.request.url.match(/^(http|https):\/\//i) )
 		return;
-	
+
 	// Return if request url is from an external domain.
 	if ( new URL(e.request.url).origin !== location.origin )
 		return;
-	
+
 	// For POST requests, do not use the cache. Serve offline page if offline.
 	if ( e.request.method !== 'GET' ) {
 		e.respondWith(
@@ -166,7 +166,7 @@ self.addEventListener('fetch', function(e) {
 		);
 		return;
 	}
-	
+
 	// Revving strategy
 	if ( e.request.mode === 'navigate' && navigator.onLine ) {
 		e.respondWith(
@@ -174,7 +174,7 @@ self.addEventListener('fetch', function(e) {
 				return caches.open(cacheName).then(function(cache) {
 					cache.put(e.request, response.clone());
 					return response;
-				});  
+				});
 			})
 		);
 		return;
@@ -186,7 +186,7 @@ self.addEventListener('fetch', function(e) {
 				return caches.open(cacheName).then(function(cache) {
 					cache.put(e.request, response.clone());
 					return response;
-				});  
+				});
 			});
 		}).catch(function() {
 			return caches.match(offlinePage);
@@ -208,11 +208,11 @@ function checkNeverCacheList(url) {
  * Register service worker
  *
  * @refer https://developers.google.com/web/fundamentals/primers/service-workers/registration#conclusion
- * 
+ *
  * @since 1.0
  */
 function superpwa_register_sw() {
-	
+
 	wp_enqueue_script( 'superpwa-register-sw', SUPERPWA_PATH_SRC . 'public/js/register-sw.js', array(), null, true );
 	wp_localize_script( 'superpwa-register-sw', 'superpwa_sw', array(
 			'url' => superpwa_sw( 'src' ),
@@ -225,7 +225,7 @@ add_action( 'wp_enqueue_scripts', 'superpwa_register_sw' );
  * Delete Service Worker
  *
  * @return true on success, false on failure
- * 
+ *
  * @since 1.0
  */
 function superpwa_delete_sw() {
@@ -234,38 +234,38 @@ function superpwa_delete_sw() {
 
 /**
  * Add images from offline page to filesToCache
- * 
- * If the offlinePage set by the user contains images, they need to be cached during sw install. 
+ *
+ * If the offlinePage set by the user contains images, they need to be cached during sw install.
  * For most websites, other assets (css, js) would be same as that of startPage which would be cached
  * when user visits the startPage the first time. If not superpwa_sw_files_to_cache filter can be used.
- * 
+ *
  * @param (string) $files_to_cache Comma separated list of files to cache during service worker install
- * 
+ *
  * @return (string) Comma separated list with image src's appended to $files_to_cache
- * 
+ *
  * @since 1.9
  */
 function superpwa_offline_page_images( $files_to_cache ) {
-	
+
 	// Get Settings
 	$settings = superpwa_get_settings();
-	
+
 	// Retrieve the post
 	$post = get_post( $settings['offline_page'] );
-	
+
 	// Return if the offline page is set to default
 	if( $post === NULL ) {
 		return $files_to_cache;
 	}
-	
+
 	// Match all images
 	preg_match_all( '/<img[^>]+src="([^">]+)"/', $post->post_content, $matches );
-	
+
 	// $matches[1] will be an array with all the src's
 	if( ! empty( $matches[1] ) ) {
 		return superpwa_httpsify( $files_to_cache . ', \'' . implode( '\', \'', $matches[1] ) . '\'' );
 	}
-	
+
 	return $files_to_cache;
 }
 add_filter( 'superpwa_sw_files_to_cache', 'superpwa_offline_page_images' );
