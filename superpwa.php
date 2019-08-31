27ae0a60dc2f8ee978f9d16bca336bc5c5fc8094
lipemat <mat@matlipe.com>
2019-08-31 17:11:08 -0400

    Bump to 2.2.0

--- superpwa.php
+++ superpwa.php
@@ -6,12 +6,12 @@
  * Author: Mat Lipe
  * Author URI: https://matlipe.com
  * Contributors: Arun Basil Lal, Jose Varghese, Mat Lipe
- * Version: 2.1.0
+ * Version: 2.2.0
  * Text Domain: super-progressive-web-apps
  * Domain Path: /languages
  * License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
-define( 'SUPERPWA_VERSION', '2.1.0' );
+define( 'SUPERPWA_VERSION', '2.2.0' );
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
